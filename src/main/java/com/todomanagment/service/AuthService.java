c87c85110292051ced420b785c2ef61fe0f67719
seid ali <40731268+seeyesuf@users.noreply.github.com>
2025-08-19 14:23:58 -0400

    test the branch protection

--- src/main/java/com/todomanagment/service/AuthService.java
+++ src/main/java/com/todomanagment/service/AuthService.java
@@ -1,7 +1,8 @@
 package com.todomanagment.service;
 
 import com.todomanagment.dto.RegisterDto;
-
+import org.springframework.stereotype.Service;
+@Service
 public interface AuthService {
     String register( RegisterDto registerDto );
 }
